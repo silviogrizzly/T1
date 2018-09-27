4479a52a1ede5a0842bbb98e3dba981bd3df7f7a
silviogrizzly <43226201+silviogrizzly@users.noreply.github.com>
2018-09-27 01:35:10 -0300

    Graficos

--- P1.py
+++ P1.py
@@ -9,3 +9,8 @@ a=(-1)*dcosx1
 
 dcosx4=(-np.cos(1.191+2*h)+8*np.cos(1.191+h)-8*np.cos(1.191-h)+np.cos(1.191-2*h))/ (12*h)
 b=(-1)*dcosx4
+
+plt.clf()
+plt.plot(h, a, color="g", label="Derivada Simple")
+plt.plot(h, b, color="b", label="Derivada a 4to Orden de error")
+plt.show()
