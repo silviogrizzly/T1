34e6d0f19bc498a7daf16c1d3d15530978e4fd0c
silviogrizzly <43226201+silviogrizzly@users.noreply.github.com>
2018-09-27 01:33:34 -0300

    Derivadas

--- P1.py
+++ P1.py
@@ -2,3 +2,10 @@
 import math
 import numpy as numpy
 import matplotlib.pyplot as plt 
+
+h=np.logspace(0,-15, 100, base=10)
+dcosx1=(np.cos(1.191+h) - np.cos(1.191)) / h
+a=(-1)*dcosx1
+
+dcosx4=(-np.cos(1.191+2*h)+8*np.cos(1.191+h)-8*np.cos(1.191-h)+np.cos(1.191-2*h))/ (12*h)
+b=(-1)*dcosx4
