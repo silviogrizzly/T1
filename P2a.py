6fc19f342b86d5377afb0721648ebf7f8437cc1d
silviogrizzly <43226201+silviogrizzly@users.noreply.github.com>
2018-09-28 22:16:45 -0300

    Ploteo

--- P2a.py
+++ P2a.py
@@ -1,4 +1,4 @@
-#Inicio P2a, creacion de script para crear listas a partir de las columnas de la matriz del archivo firas_monopole_spec_v1.txt
+#Inicio P2a, creacion de script para crear arrays a partir de las columnas de la matriz del archivo firas_monopole_spec_v1.txt
 import numpy as np
 
 File="firas_monopole_spec_v1.txt"
@@ -12,3 +12,11 @@ Spec = Data[:,1]
 ResS = Data[:,2]
 UncS = Data[:,3]
 GalS = Data[:,4]
+
+
+fig, ax = plt.subplots()
+ax.errorbar(Frec, Spec , yerr=UncS, label='Espectro FIRAS')
+plt.xlabel('Frecuencia [Hz]')
+plt.ylabel('Radiacion [MJy/sr]')
+plt.legend()
+plt.savefig('espectro.png')
