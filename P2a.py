dd953391f0b1c92d16e59f5a23e295c250b08a5d
silviogrizzly <43226201+silviogrizzly@users.noreply.github.com>
2018-09-28 22:15:46 -0300

    Creacion arrays

--- P2a.py
+++ P2a.py
@@ -2,3 +2,13 @@
 import numpy as np
 
 File="firas_monopole_spec_v1.txt"
+
+Data= np.loadtxt(File)
+
+Data = np.array(Data)
+
+Frec = Data[:,0]
+Spec = Data[:,1]
+ResS = Data[:,2]
+UncS = Data[:,3]
+GalS = Data[:,4]
