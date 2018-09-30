3d35255fda898dcce15d73ecc014980c69cbe12e
silviogrizzly <43226201+silviogrizzly@users.noreply.github.com>
2018-09-29 21:17:28 -0300

    Variables y funcion*
    La funcion f(x) es la funcion del problema luego de un cambio de variable y=arctan(x)

--- P2b.py
+++ P2b.py
@@ -4,3 +4,17 @@ from astropy import constants as const
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
+m=30
+N=3*m
+a=0
+b=np.pi/2
+h=(np.pi/(2*(N)))
+
+P_an= (np.pi**(4))/15
+
+def f(x):         #Funcion a integrar con el cambio de variable ya realizado
+    n=(math.tan(x))**3
+    d=(np.exp(math.tan(x))-1)*(math.cos(x)**2)
+    y=n/d
+    return y
