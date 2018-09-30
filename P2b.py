64df00aef93fc9029982349fc61869d71c356001
silviogrizzly <43226201+silviogrizzly@users.noreply.github.com>
2018-09-29 21:19:31 -0300

    Testeo metodos de integracion
    Se agregaron los metodos de integracion y su testeo, logrando un resultado mejor con el metodo trapezoidal

--- P2b.py
+++ P2b.py
@@ -18,3 +18,50 @@ def f(x):         #Funcion a integrar con el cambio de variable ya realizado
     d=(np.exp(math.tan(x))-1)*(math.cos(x)**2)
     y=n/d
     return y
+
+#Metodo trapezoidal
+    
+Suma=0
+Sumb=0
+
+x=a
+for i in range(1, N):
+    Suma=Suma+f(x+i*h)
+for i in range(2, N-1):
+    Sumb=Sumb+f(x+i*h)
+
+It=(h/2)*(Suma+Sumb)
+
+
+#Metodo Simpson 1/3
+
+
+Spar=0
+Simpar=0
+
+n=(N/2)-1
+for i in range(1, n):
+    Spar=Spar+f(x+(2*i)*h)
+for i in range(0, n):
+    Simpar=Simpar+f(x+(2*i+1)*h)
+
+Isu=(h/3)*(4*Simpar+2*Spar)
+    
+#Metodo de Simpson 3/8
+
+Su=0
+Sd=0
+
+for i in range(0, m-1):
+    Su=Su+f(x+(3*i+1)*h)+f((3*i+2)*h)
+for i in range(1, m-1):
+    Sd=Sd+f(x+3*i*h)
+    
+Ist=(3*h/8)*(3*Su+2*Sd)
+
+print It
+print Isu
+print Ist
+print P_an
+
+#Metodo elegido: Metodo trapezoidal ya que con menor numero de particiones, se consigue mayor exactitud
